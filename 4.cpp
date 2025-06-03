0e6b66f3108fb0bcecb1af98236857f5919854bf
winterwntend <canilalm@yandex.ru>
2025-06-03 11:03:01 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,1 +1,47 @@
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double func(double x) {
+    return x * x * exp(-x);
+}
+
+double trapezoid_rule(double (*func)(double), double a, double b, int nseg) {
+    double dx = (b - a) / nseg;
+    double sum = 0.5 * (func(a) + func(b));
+
+    for (int i = 1; i < nseg; i++) {
+        sum += func(a + i * dx);
+    }
+
+    return sum * dx;
+}
+
+int main() {
+    cout << "Используем метод трапеций" << endl;
+    cout << "Интегрируемая функция: f(x) = x^2 * e^(-x)" << endl;
+
+    double a = 0;
+    double b = 1.0; 
+    int nseg = 2;
+    double eps = 1e-6;
+
+    cout << "a = " << a << " , b = " << b << " , eps = " << eps << endl;
+
+    double int_1 = trapezoid_rule(func, a, b, nseg);
+    nseg *= 2;
+    double int_2 = trapezoid_rule(func, a, b, nseg);
+
+    while (fabs(int_1 - int_2) > eps) {
+        nseg *= 2;
+        int_1 = trapezoid_rule(func, a, b, nseg);
+        nseg *= 2;
+        int_2 = trapezoid_rule(func, a, b, nseg);
+    }
+
+    cout << "\nОтвет: I = " << int_2 << "\nКоличество разбиений: " << nseg << endl;
+
+    return 0;
+}
